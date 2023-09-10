4ea12692af3d405a7c4ffb73d8e536d5580f3888
Breezy21st <110764642+Breezy21st@users.noreply.github.com>
2023-09-10 23:17:06 +0200

    new file:   .env 	modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	modified:   views/index.html

--- index.js
+++ index.js
@@ -24,9 +24,63 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// Define a route for /api/:timestamp
+app.get('/api/:timestamp', (req, res) => {
+  const { timestamp } = req.params;
+
+  // Parse the timestamp as an integer
+  const unixTimestamp = parseInt(timestamp);
+
+  // Check if the parsed timestamp is a valid number
+  if (isNaN(unixTimestamp)) {
+    return res.json({ error: 'Invalid Unix Timestamp' });
+  }
+
+  // Convert Unix timestamp to a Date object
+  const dateObject = new Date(unixTimestamp);
+
+  // Check if the date is valid
+  if (!isNaN(dateObject.getTime())) {
+    return res.json({ unix: unixTimestamp, utc: dateObject.toUTCString() });
+  } else {
+    return res.json({ error: 'Invalid Date' });
+  }
+});
+
+// Define a route for /api/:date?
+app.get('/api/:date?', (req, res) => {
+  const { date } = req.params;
+
+  // Handle empty date parameter (current time)
+  if (!date) {
+    const currentTime = new Date();
+    return res.json({ unix: currentTime.getTime(), utc: currentTime.toUTCString() });
+  }
+
+  // Parse the date string using date-fns
+  const dateObject = parseISO(date);
+
+  // Check if the date is valid
+  if (isValid(dateObject)) {
+    return res.json({ unix: dateObject.getTime(), utc: dateObject.toUTCString() });
+  } else {
+    return res.json({ error: 'Invalid Date' });
+  }
+});
+
+
+
+// Error handling middleware for handling uncaught errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
